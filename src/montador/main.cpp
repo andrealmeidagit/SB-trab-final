37b5482a95ba65aa4f48fe0a1c7b059f2938ba2e
Andre <andre.almeida3@gmail.com>
2019-02-04 19:25:00 -0200

    first steps
    baby steps

---
+++ src/montador/main.cpp
@@ -0,0 +1,17 @@
+/*
+Universidade de Brasilia - 01/2019
+CIC 116432 - Software Basico - Turma B
+Professor Bruno Macchiavello
+Trabalho Pratico 1 - Montador + Ligador
+
+Aluno: Andre Abreu Rodrigues de Almeida    12/0007100
+*/
+
+
+using namespace std;
+
+int main (int argc, char** argv){
+
+    
+    return 0;
+}
